51a4f09f719fb5498e7ed7074c3c473390b08a96
solvep <124667390@qq.com>
2021-04-01 21:37:20 +0800

    css

--- javaScript基础/继承.js
+++ javaScript基础/继承.js
@@ -45,32 +45,59 @@
 // 组合继承
 
 function parent3() {
-  this.name = 'parent3'
-  this.play = [1, 2, 3]
+  this.name = "parent3";
+  this.play = [1, 2, 3];
 }
 
-parent3.prototype.getName = function (){
-  return this.name
-}
-parent3.prototype.test = [1, 2, 3, 4, 5]
+parent3.prototype.getName = function () {
+  return this.name;
+};
+parent3.prototype.test = [1, 2, 3, 4, 5];
 
-
-parent3.prototype.nickName = '123'
+parent3.prototype.nickName = "123";
 
 function child3() {
-  parent3.call(this)
-  this.type = 'child3'
+  parent3.call(this);
+  this.type = "child3";
 }
 
-child3.prototype = new parent3()
+child3.prototype = new parent3();
 
-child3.prototype.constructor = child3
+child3.prototype.constructor = child3;
 
-let s1 = new child3()
-let s2 = new child3()
+let s1 = new child3();
+let s2 = new child3();
 
-s2.name = 1
-s2.test.push('090')
-s2.play.push('11111')
+s2.name = 1;
+s2.test.push("090");
+s2.play.push("11111");
 
-console.log('-----', s1.test, s2.test, s1.nickName, s2.nickName)
+console.log("-----", s1.test, s2.test, s1.nickName, s2.nickName);
+
+/* 请在下面写出JavaScript面向对象编程的混合式继承。并写出ES6版本的继承。
+要求：汽车是父类，Cruze是子类。父类有颜色、价格属性，有售卖的方法。
+Cruze子类实现父类颜色是红色，
+价格是140000,售卖方法实现输出如下语句：将 红色的Cruze买给了小王价格是14万。 */
+
+function car() {
+  this.color = "blue";
+  this.price = "10";
+  this.name = "car";
+}
+car.prototype.method = function () {
+  console.log(`将${this.color}的${this.name}买给了小王价格是${this.price}。`);
+};
+
+function cruze() {
+  car.call(this);
+  this.name = "cruze";
+  this.color = "红色";
+  this.price = "14万";
+}
+
+// cruze.prototype = new car()
+cruze.prototype = Object.create(car.prototype);
+cruze.prototype.constructor = cruze;
+console.log("cruze.prototype", cruze.prototype);
+let c = new cruze();
+c.method();
